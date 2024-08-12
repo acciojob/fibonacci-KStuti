2c4cbbebfbe55a56881d0a49d89745d129299414
Stuti Kaushik <kaushikstuti4@gmail.com>
2024-08-12 15:29:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 function fibonacci(num) {
 // your code here
-	if(n===1){
-		return 0
+	
+    if (num <= 0){
+        return "Invalid input. Please provide a positive integer."
 	}
-	if(n===5){
-		return 3
+    if (num == 1){
+        return 0
 	}
+    if (num == 2){
+        return 1
+	}
+    else{
+        a, b = 0, 1
+        for _ in range(2, num)
+            a, b = b, a + b
+        return b
+		}
+print(fibonacci(1))  
+print(fibonacci(5))  
 	
 }
 
