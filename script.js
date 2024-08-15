0002cb60e76f36e253656493dcb8fea12baaed1d
Stuti Kaushik <kaushikstuti4@gmail.com>
2024-08-15 15:27:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,14 @@
-function fibonacci(num) {
-// your code here
-	
-    if (num <= 0){
-        return "Invalid input. Please provide a positive integer."
-	}
-    if (num == 1){
-        return 0
-	}
-    if (num == 2){
-        return 1
-	}
-    else{
-        a, b = 0, 1
-        for _ in range(2, num)
-            a, b = b, a + b
-        return b
-		}
-print(fibonacci(1))  
-print(fibonacci(5))  
-	
+function fibonacci(N)
+{
+let a = 0; 
+let b = 1;  
+let c = 0; 
+let currentPosition = 3;
+while (currentPosition <= N) { 
+    c = a + b;
+     a = b; 
+      b = c; 
+    ++currentPosition;
 }
-
-module.exports = fibonacci;
+return c;
+}
